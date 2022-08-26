8025a1210aad4c42df3b84d66ef5114728c84b72
saienko-alex <saenkops3@gmail.com>
2022-08-26 17:38:28 +0300

    task1 done

---
+++ task1/js/script.js
@@ -0,0 +1,6 @@
+let firstNumber = prompt('Напишите первое число?');
+let secondNumber = prompt('Напишите второе число?');
+let result = +firstNumber + +secondNumber;
+
+
+alert(`${firstNumber} + ${secondNumber} = ${result}`);
