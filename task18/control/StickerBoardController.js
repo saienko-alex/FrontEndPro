4e7e95f5366e77023cb1e86c0accf762b1a01cfb
saienko-alex <saenkops3@gmail.com>
2022-11-16 22:42:02 +0200

    task18 fixed

--- task18/control/StickerBoardController.js
+++ task18/control/StickerBoardController.js
@@ -7,19 +7,27 @@ class StickerBoardController {
         this.#headerView = new Header({});
         container.append(this.#headerView.el)
 
-        this. #sctickerBoardView = new StickerBoard({});
-        container.append(this. #sctickerBoardView.el)
+        this. #sctickerBoardView = new StickerBoard({
+            onDelete: (id) => this.deleteSticker(id),
+            onUpdate: (id, values) => this.updateSticker(id, values),
+        });
+        container.append(this.#sctickerBoardView.el)
 
 
 
         this.#collection = new StickerBoardCollection();
         this.#collection.fetchList().then(() => {
-            // this.#sctickerBoardView.renderList(this.#collection.list)
+            this.#sctickerBoardView.renderList(this.#collection.list)
         });   
     }
     deleteSticker(id) {
         this.#collection.deleteSticker(id).then(() => {
-            // this.#sctickerBoard.renderList(this.#collection.list)
+            this.#sctickerBoardView.renderList(this.#collection.list)
+        });
+    }
+    updateSticker(id, values) {
+        this.#collection.updateSticker(id, values).then(() => {
+            this.#sctickerBoardView.renderList(this.#collection.list)
         });
     }
 }    
--- task18/view/StickerBoard.js
+++ task18/view/StickerBoard.js
@@ -2,16 +2,32 @@ class StickerBoard {
     #config = null;
     el = null
 
+    static CLASSES = {
+        ITEMS: 'sticker-block',
+        CLOSE: 'close-button',
+        TEXTAREA: 'edit-control',
+
+    };
+
+
+    static iTemTemplate = `
+    <div id = "mainform" class="mainform">
+    `;
+
 
     static stickerITemTemplate = `
-    //     <div class="sticker-block">
-    //         <div class="header-block">
-    //             <button type="button" class="close-button">X</button>
-    //         </div>
-    //         <textarea>
-    //         </textarea>
-    //     </div> 
-    //     `;
+        <div class="sticker-block" data-sticker-id="{{id}}">
+            <div class="header-block">
+                <button type="button" class="close-button">X</button>
+            </div>
+            <textarea class="edit-control">{{description}}</textarea>
+        </div> 
+    `;
+
+    static generateStickersHtml(sticker) {
+        return interpolate(StickerBoard.stickerITemTemplate, sticker)
+
+    }
 
     static htmlToElement(html) {
         const container = document.createElement('div');
@@ -21,6 +37,12 @@ class StickerBoard {
         return container.children[0];   
     } 
 
+    static getStickerId(el) {
+        const parent = el.closest('.' + StickerBoard.CLASSES.ITEMS);
+
+        return parent ? parent.dataset.stickerId : null; 
+    }
+
     constructor(config) {
         this.#config = config;
         this.#initView();
@@ -29,14 +51,67 @@ class StickerBoard {
 
     #initView() {
 
-        this.el = StickerBoard.htmlToElement(StickerBoard.stickerITemTemplate);
+        this.el = StickerBoard.htmlToElement(StickerBoard.iTemTemplate);
 
-        // this.addButtonEl.addEventListener('click', () => {
-        //     console.log('hello')
-        // });   
+
+        this.el.addEventListener('click', (e) => {
+            const stickerId = StickerBoard.getStickerId(e.target);
+
+            switch (true) {
+                case e.target.classList.contains(
+                    StickerBoard.CLASSES.CLOSE
+                ):
+                    return this.deleteSticker(stickerId);
+            
+            }
+        });
+        this.el.addEventListener('change', (e) => {
+            const stickerId = StickerBoard.getStickerId(e.target);
+            switch (true) {
+                case e.target.classList.contains(
+                    StickerBoard.CLASSES.TEXTAREA
+                ):
+                    return this.changeSticker(stickerId, e.target.value);
+            }        
+        });
 
     }
+    renderList(list) {
+        this.el.innerHTML = list
+            .map(StickerBoard.generateStickersHtml)
+            .join('');
+    }
+    deleteSticker(id) {
+        this.#config.onDelete(id);
+    }
+    changeSticker(id, value) {
+        this.#config.onUpdate(id, { description: value });
+    }
+
 }
+
+function interpolate (template, obj) {
+    for (key in obj){
+        template = template.replaceAll(`{{${key}}}`, obj[key])
+    }
+    return template;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
