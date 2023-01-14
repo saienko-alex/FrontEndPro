051026d8c5947342226c040ee7aecd27454af6d1
saienko-alex <saenkops3@gmail.com>
2023-01-14 22:13:41 +0200

    task28

--- task28/my-app/src/components/App/App.js
+++ task28/my-app/src/components/App/App.js
@@ -1,33 +1,12 @@
 import './App.css';
 
-import { useEffect, useState } from 'react';
+
 
 import Form from '../Form/Form';
 import List from '../List/List';
 
 function App() {
-    // function toggleTodo(id) {
-    //     setList(
-    //         list.map((item) =>
-    //             item.id === id ? { ...item, isDone: !item.isDone } : item
-    //         )
-    //     );
-    // }
-
-    // function deleteTodo(id) {
-    //     setList(list.filter((item) => item.id !== id));
-    // }
-
-    // function saveTodo(newTodo) {
-    //     setList([
-    //         ...list,
-    //         {
-    //             id: Date.now(),
-    //             ...newTodo,
-    //             isDone: false,
-    //         },
-    //     ]);
-    // }
+  
 
     return (
         <div className="container">
